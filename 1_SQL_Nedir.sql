3561c4c12536e3ec1ed034766d98100bf204c439
yoncademirhan <99687715+yoncademirhan@users.noreply.github.com>
2022-09-15 23:07:19 +0300

    Add files via upload

---
+++ 1_SQL_Nedir.sql
@@ -0,0 +1,17 @@
+
+--Sql'de yorum satýrýný iki adet (-) ile yapýlýyor.
+--Burada yazýlan yorum satýrlarýný derleyici görmez ve iþlem yapmaz.
+
+--Veritabaný genellikle bir bilgisayar sisteminde elektronik olarak depolanan yapýlandýrýlmýþ bilgi veya veriden oluþan düzenli bir koleksiyondur.
+
+--Sql(Structured Query Language) nedir ? 
+
+--SQL verileri sorgulamak, deðiþtirmek ve tanýmlamak ve ayný zamanda eriþim kontrolü saðlamak üzere neredeyse tüm iliþkisel veritabanlarýnda kullanýlan bir programlama dilidir.
+
+--SQL'in türkçe karþýlýðý Yapýlandýrýlýrmýþ Sorgu Dili'dir.
+
+--SQL temelde 3 alt ifade grubundan oluþur.
+
+-- DDL (Data Definition Language): Veri tanýmlama deyimleri.
+-- DML (Data Manuplation Language) : Veri düzenleme dili.
+-- DCL (Data Control Language): Veri kontrol dili.
